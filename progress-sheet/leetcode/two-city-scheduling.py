24e875868ce9bb00680a3e2df36d244bf9c46318
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-02-23 19:41:04 +0300

    Add solution for Two City Scheduling

--- progress-sheet/leetcode/two-city-scheduling.py
+++ progress-sheet/leetcode/two-city-scheduling.py
@@ -1,24 +1,21 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        a = 0
-        b = 0
-        ca = 0
-        cb = 0
-        costs.sort(key= lambda x:abs(x[0]-x[1]), reverse=True)
-        half=len(costs)//2
-        for m,n in costs:
-            if m>n:
-                if cb<half:
-                    b+=n
-                    cb+=1
-                else:
-                    a+=m
+        costs.sort(key= lambda x: abs(x[0]-x[1]), reverse=True)
+        n = len(costs)//2
+        a, b, ca, cb = 0, 0, 0, 0
+        for x,y in costs:
+            if x<y:
+                if ca<n:
+                    a+=x
                     ca+=1
+                else:
+                    b+=y
+                    cb+=1
             else:
-                if ca<half:
-                    a+=m
-                    ca+=1
-                else:
-                    b+=n
+                if cb<n:
+                    b+=y
                     cb+=1
+                else:
+                    a+=x
+                    ca+=1
         return a+b
